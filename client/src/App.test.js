485936170bcd667f0c5e46785b97ea87b870400e
Armin <a_h82@gmx.net>
2021-08-15 12:52:29 +0200

    nn

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn app title', () => {
   render(<App />);
-  
   const linkElement = screen.getByText(/training plan/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn login link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/login/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn registry link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/registrierung/i);
   expect(linkElement).toBeInTheDocument();
 });
